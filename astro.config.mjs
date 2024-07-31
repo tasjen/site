dd27e8fce9adf70cad441b363d51978f70a01e01
tasjen <129994991+tasjen@users.noreply.github.com>
2024-07-31 07:20:08 +0700

    Add Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+import icon from "astro-icon";
+
 export default defineConfig({
   site: "https://tasjen.github.io",
+  integrations: [tailwind(), icon()],
 });
