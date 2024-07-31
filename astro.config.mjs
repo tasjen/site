c959c6cf1d764a04a57c2403c1bdb00f76c35aad
tasjen <129994991+tasjen@users.noreply.github.com>
2024-07-31 09:12:45 +0700

    Fix css problem from GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import icon from "astro-icon";
 export default defineConfig({
   site: "https://www.tasjen.pro",
   integrations: [tailwind(), icon()],
+  build: {
+    assetsPrefix: "https://www.tasjen.pro",
+  },
 });
